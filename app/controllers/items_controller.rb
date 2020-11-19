a37ccf4c423d06f0d749b8a446a4675da539135d
Ryo-Nogawa <69405100+Ryo-Nogawa@users.noreply.github.com>
2020-11-19 11:03:27 +0900

    エラーメッセージを表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,24 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.valid?
+      @user.save
+      redirect_to action: :index
+    else
+      render action: :new
+    end
+  end
+
+
+  private
+  def user_params
+    params.require(:user).permit(:nickname, :last_name, :first_name, :kana_last_name, :kana_first_name, :birthday)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  
+  validates :password,        format: {with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}+\z/i}
   validates :nickname,        presence: true
-  validates :last_name,       presence: true
-  validates :first_name,      presence: true
-  validates :kana_last_name,  presence: true
-  validates :kana_first_name, presence: true
+  validates :last_name,       presence: true, format: {with: /\A[一-龥ぁ-ん]/}
+  validates :first_name,      presence: true, format: {with: /\A[一-龥ぁ-ん]/}
+  validates :kana_last_name,  presence: true, format: {with: /\A[ァ-ヶー－]+\z/}
+  validates :kana_first_name, presence: true, format: {with: /\A[ァ-ヶー－]+\z/}
   validates :birthday,        presence: true
 end
+
+
