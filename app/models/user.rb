295b8d87dcae2de425427dab3ddf3eadb55fe450
Ryo-Nogawa <69405100+Ryo-Nogawa@users.noreply.github.com>
2020-11-19 15:12:38 +0900

    バリデーションをwith_optionsでまとめる

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  
   validates :password,        format: {with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}+\z/i}
-  validates :nickname,        presence: true
-  validates :last_name,       presence: true, format: {with: /\A[一-龥ぁ-ん]/}
-  validates :first_name,      presence: true, format: {with: /\A[一-龥ぁ-ん]/}
-  validates :kana_last_name,  presence: true, format: {with: /\A[ァ-ヶー－]+\z/}
-  validates :kana_first_name, presence: true, format: {with: /\A[ァ-ヶー－]+\z/}
-  validates :birthday,        presence: true
+  with_options presence: true do
+    validates :nickname
+    validates :last_name,       format: {with: /\A[一-龥ぁ-ん]/}
+    validates :first_name,      format: {with: /\A[一-龥ぁ-ん]/}
+    validates :kana_last_name,  format: {with: /\A[ァ-ヶー－]+\z/}
+    validates :kana_first_name, format: {with: /\A[ァ-ヶー－]+\z/}
+    validates :birthday
+  end
 end
 
 
