00b9c8a3681f522b92e014672ad5829086b3e602
Ryo-Nogawa <69405100+Ryo-Nogawa@users.noreply.github.com>
2020-11-19 19:03:28 +0900

    categoriesモデル作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,14 +4,15 @@ class Item < ApplicationRecord
     validates :image
     validates :title,             length: {maximum: 40}
     validates :detail,            length: {maximum: 1000}
-    validates :category_id
+    validates :category_id,       numericality: { other_than: 1}
     validates :item_condition_id
     validates :item_cost_id
     validates :delivery_area_id 
     validates :delivery_date_id
-    validates :price,            numericality: {only_integer: true, greater_than: 300, less_than: 9999999}, format: {with: /\A[0-9]+\z/ }
+    validates :price,            numericality: { only_integer: true, greater_than: 300, less_than: 9999999}, format: {with: /\A[0-9]+\z/ }
   end
 
   belongs_to :user
   has_one_attached :image
+  belongs_to :category
 end
