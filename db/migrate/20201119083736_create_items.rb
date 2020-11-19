7cacddcd1435e5ede25c63f52d839d8343b7a69c
Ryo-Nogawa <69405100+Ryo-Nogawa@users.noreply.github.com>
2020-11-19 17:49:10 +0900

    マイグレーションファイル作成

--- db/migrate/20201119083736_create_items.rb
+++ db/migrate/20201119083736_create_items.rb
@@ -1,7 +1,15 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-
+      t.references :user,              foreign_key: true
+      t.string     :title,             null: false
+      t.text       :detail,            null: false
+      t.integer    :category_id,       null: false
+      t.integer    :item_condition_id, null: false
+      t.integer    :item_cost_id,      null: false
+      t.integer    :delivery_area_id,  null: false
+      t.integer    :delivety_date_id,  null: false
+      t.integer    :price,             null: false
       t.timestamps
     end
   end
