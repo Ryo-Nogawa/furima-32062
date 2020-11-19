d2d7537ab9bf6a134e850e945e763fce46fe8b44
Ryo-Nogawa <69405100+Ryo-Nogawa@users.noreply.github.com>
2020-11-19 15:16:07 +0900

    rubocopで自動整形

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,15 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :password,        format: {with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}+\z/i}
+  validates :password,        format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}+\z/i }
   with_options presence: true do
     validates :nickname
-    validates :last_name,       format: {with: /\A[一-龥ぁ-ん]/}
-    validates :first_name,      format: {with: /\A[一-龥ぁ-ん]/}
-    validates :kana_last_name,  format: {with: /\A[ァ-ヶー－]+\z/}
-    validates :kana_first_name, format: {with: /\A[ァ-ヶー－]+\z/}
+    validates :last_name,       format: { with: /\A[一-龥ぁ-ん]/ }
+    validates :first_name,      format: { with: /\A[一-龥ぁ-ん]/ }
+    validates :kana_last_name,  format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :kana_first_name, format: { with: /\A[ァ-ヶー－]+\z/ }
     validates :birthday
   end
 end
-
-
