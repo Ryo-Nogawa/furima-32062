79a7ca492ea0cdaa3321201d1bf4cb0ef752856b
Ryo-Nogawa <69405100+Ryo-Nogawa@users.noreply.github.com>
2020-11-20 12:44:13 +0900

    with_optionsを使ってコードをまとめる

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,11 +3,13 @@ class Item < ApplicationRecord
     validates :image
     validates :title,             length: { maximum: 40 }
     validates :detail,            length: { maximum: 1000 }
-    validates :category_id,       numericality: { other_than: 1 }
-    validates :item_condition_id, numericality: { other_than: 1 }
-    validates :item_cost_id,      numericality: { other_than: 1 }
     validates :delivery_area_id,  numericality: { other_than: 0 }
-    validates :delivery_date_id,  numericality: { other_than: 1 }
+    with_options numericality: { other_than: 1 } do
+      validates :category_id
+      validates :item_condition_id
+      validates :item_cost_id
+      validates :delivery_date_id
+    end
     validates :price, numericality: { only_integer: true, greater_than: 300, less_than: 9_999_999 }, format: { with: /\A[0-9]+\z/ }
   end
 
