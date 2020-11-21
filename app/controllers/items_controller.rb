52bbbe65df23c4664e21f6aacf4d389563d067a8
Ryo-Nogawa <69405100+Ryo-Nogawa@users.noreply.github.com>
2020-11-21 12:27:47 +0900

    rubocop

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -35,9 +35,7 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    if current_user.id == @item.user_id && @item.destroy
-      redirect_to root_path
-    end
+    redirect_to root_path if current_user.id == @item.user_id && @item.destroy
   end
 
   private
