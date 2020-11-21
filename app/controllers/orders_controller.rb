e9745135c90cab040e191ba2dc9f3884bf00fa8a
Ryo-Nogawa <69405100+Ryo-Nogawa@users.noreply.github.com>
2020-11-22 01:11:18 +0900

    ordersコントローラー作成

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,26 @@
 class OrdersController < ApplicationController
-  
+
   def index
+    @item = Item.find(params[:item_id])
+  end
+
+  def new
+    @order_address = OrderAddress.new
   end
 
   def create
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to action: :index
+    else
+      render action: :new
+    end
   end
+
+  private
+  def order_params
+    params.require(:order_address).permit(:order_id, :posta_code, :prefectur_id, :city, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: @item.id)
+  end
+
 end
