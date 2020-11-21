867513377d30e8ee60c8bf2500396ade559cfe18
Ryo-Nogawa <69405100+Ryo-Nogawa@users.noreply.github.com>
2020-11-21 09:17:38 +0900

    アクセス制限

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit]
+
   def index
     @items = Item.includes(:user).order('created_at DESC')
   end
@@ -23,6 +24,9 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
+    unless  current_user.id == @item.user_id
+      redirect_to root_path
+    end
   end
 
   def update
@@ -39,4 +43,5 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:image, :title, :detail, :category_id, :item_condition_id, :item_cost_id, :delivery_area_id, :delivery_date_id, :price).merge(user_id: current_user.id)
   end
+
 end
