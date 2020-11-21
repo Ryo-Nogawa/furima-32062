f84ad445b25bec1bb1605cceeeaac6af61bf9975
Ryo-Nogawa <69405100+Ryo-Nogawa@users.noreply.github.com>
2020-11-21 11:41:24 +0900

    deleteアクションを追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit]
-  before_action :set_action, only: [:show, :edit, :update]
+  before_action :set_action, only: [:show, :edit, :updat]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -32,6 +32,11 @@ class ItemsController < ApplicationController
     else
       render :edit
     end
+  end
+
+  def destroy
+    item = Item.find(params[:id])
+    item.destroy
   end
 
   private
