ae66536f43e06f084d46a674a88b5d011bfd78dd
Ryo-Nogawa <69405100+Ryo-Nogawa@users.noreply.github.com>
2020-11-24 13:25:23 +0900

    order_addressコントローラー作成

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,25 +2,34 @@ class OrdersController < ApplicationController
 
   def index
     @item = Item.find(params[:item_id])
-  end
-
-  def new
     @order_address = OrderAddress.new
   end
 
   def create
-    @order_address = OrderAddress.new(order_params)
+    @item = Item.find(params[:item_id])
+    @order_address = OrderAddress.new(order_address_params)
     if @order_address.valid?
       @order_address.save
-      redirect_to action: :index
+      pay_item
+      redirect_to  root_path
     else
-      render action: :new
+      # redirect_to item_orders_path(params[:item_id])
+      render action: :index
     end
   end
 
   private
-  def order_params
-    params.require(:order_address).permit(:order_id, :posta_code, :prefectur_id, :city, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: @item.id)
+  def order_address_params
+    params.require(:order_address).permit(:postal_code, :prefectual_id, :city, :house_number, :building_name, :phone_number).merge(token: params[:token], user_id: current_user.id, item_id: @item.id)
+  end
+
+  def pay_item
+    Payjp.api_key = 'sk_test_1b11a596bdd2b55fdd22d887'
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_address_params[:token],
+      currency: 'jpy'
+    )
   end
 
 end
