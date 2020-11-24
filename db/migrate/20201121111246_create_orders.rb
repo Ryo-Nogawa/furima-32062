c2eef16cea11eb7ebe44b399bb936ba674d75ee7
Ryo-Nogawa <69405100+Ryo-Nogawa@users.noreply.github.com>
2020-11-24 17:38:55 +0900

    マイグレーションファイルのスペルミス

--- db/migrate/20201121111246_create_orders.rb
+++ db/migrate/20201121111246_create_orders.rb
@@ -1,8 +1,8 @@
 class CreateOrders < ActiveRecord::Migration[6.0]
   def change
     create_table :orders do |t|
-      t.referencrs :user,    null: false, foreign_key: true
-      t.referencrs :item_id, null: false, foreign_key: true
+      t.references :user,    null: false, foreign_key: true
+      t.references :item_id, null: false, foreign_key: true
       t.timestamps
     end
   end
