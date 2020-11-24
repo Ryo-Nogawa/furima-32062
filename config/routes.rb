1a4c47c8864d4bf55a4ddf1d05ffdbac2cbf3731
Ryo-Nogawa <69405100+Ryo-Nogawa@users.noreply.github.com>
2020-11-24 14:51:12 +0900

    削除機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
+<<<<<<< Updated upstream
   resources :items
+=======
+  resources :items do
+    resources :orders, only: [:index, :create]
+  end
+>>>>>>> Stashed changes
 end
