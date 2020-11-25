2a5cf267a6262be484885146c854093298ed5aa1
Ryo-Nogawa <69405100+Ryo-Nogawa@users.noreply.github.com>
2020-11-25 11:54:53 +0900

    rubocop

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,14 +3,11 @@ class OrdersController < ApplicationController
   before_action :set_action, only: [:index, :create]
   before_action :redirect_index, only: :index
 
-
   def index
     @order_address = OrderAddress.new
     # if current_user.id == @item.user_id && @item.order != nil
     # if @item.order != nil && current_user.id == @item.user_id
-    if @item.order != nil
-      redirect_to root_path
-    end
+    redirect_to root_path unless @item.order.nil?
   end
 
   def create
