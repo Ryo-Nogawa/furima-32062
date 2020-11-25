7084a9602e7024c0b38ea59b64d6c33da3251668
Ryo-Nogawa <69405100+Ryo-Nogawa@users.noreply.github.com>
2020-11-25 11:53:30 +0900

    売却済みの商品にアクセス制限を設ける

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,10 +1,16 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!, only: [:index]
   before_action :set_action, only: [:index, :create]
+  before_action :redirect_index, only: :index
+
 
   def index
     @order_address = OrderAddress.new
-    redirect_to root_path if current_user.id == @item.user_id
+    # if current_user.id == @item.user_id && @item.order != nil
+    # if @item.order != nil && current_user.id == @item.user_id
+    if @item.order != nil
+      redirect_to root_path
+    end
   end
 
   def create
@@ -36,5 +42,9 @@ class OrdersController < ApplicationController
 
   def set_action
     @item = Item.find(params[:item_id])
+  end
+
+  def redirect_index
+    current_user.id == @item.user_id
   end
 end
